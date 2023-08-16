eec670073f3751cc1551c04268ef35af9c833288
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-08-16 14:04:51 -0300

    removi imports d eoutros temas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+ import data from './data/tarot/tarot.js';
 
 console.log(example, data);
+
+let player = function(){
+    //chamar pelo id
+    
+    }
+    
+    
+    //mostrar um card com todas as funcionalidades:
