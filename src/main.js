db3135c39014774e8f40a7cdbf12a7e9e0bc665d
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-08-31 08:37:31 -0300

    cartas aparecendo no html

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // searchByName,
 // filterArcane,
 
-import * as data from './data/tarot/tarot.js' ;
+import data from './data/tarot/tarot.js' ;
 
 // //mostrar a carta no html
 const cardsContainer = document.querySelector("#cards-container");//chamando pela classe usa o . e pelo id #
@@ -10,49 +10,24 @@ const dataTarot = data.cards;
 function exposeCards(data){
 
   const exposeCards = document.querySelector("#expose-card");
-  exposeCards.innerHTML += "";
-  data.forEach((dataTarot) => {
+  exposeCards.innerHTML = "";
+  data.forEach((tarot) => {
     const container = document.createElement("div");
     container.classList.add("show-img");
-    const showCards += 
+    const showCards = 
     `<div class="show-img">
-    <img src="${dataTarot.img}" alt="Card Image">
+    <img src="${tarot.img}" alt="Card Image">
     </div>`;
 
     container.innerHTML += showCards
     exposeCards.appendChild(container)
-    cardsContainer.innerHTML += container
+    
 
-});
+  });
+  cardsContainer.innerHTML += exposeCards
 }
 
 window.addEventListener("load", () => {
   exposeCards(dataTarot)
 
-});
-
-// console.log(cards-container);
-// console.log(data.default.cards);
-// console.log(data.default.cards[0]);
-
-// cardsContainer.innerHTML= 
-// `<h1 class="title">${data.default.cards[0].name}</h1>
-//  <img src="${data.default.cards[0].img}">`; 
-
-
-
-// let player = function(){
-//     //chamar pelo idcha
-//mostrar um card com todas as funcionalidades:
-
-// function containerWithCards(data) {
-//   const cardsContainer = document.querySelector("#cards-container");
-//   cardsContainer.innerHTML = data.map(
-//     (cards) =>
-//       `<li class="card">
-// <h4 class="arcane-name">${cards.name}</h4>
-// <img class="show-img" src=${cards.img}/>
-// </li>`
-//   )
-//     .join("");
-// }
+})
